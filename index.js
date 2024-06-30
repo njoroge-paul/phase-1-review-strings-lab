ebb80c724ace36ffae09fffef4a2dedda407ef30
Duncan <njorogedun870@gmail.com>
2024-06-30 23:23:56 +0300

    added strings

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook'+ currentUser;
+
+const currentUser2 = 'Grace Hopper';
+
+const welcomeMessage2 = `welcome to Flatbook , ${currentUser}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
